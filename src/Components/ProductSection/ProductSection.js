d3c7357f3540a36698b43f2b5f1881fd6519c197
Parvez Musharraf <parvez.musharraf@pixonix.tech>
2024-07-27 19:44:32 +0530

    Product section error handled

--- src/Components/ProductSection/ProductSection.js
+++ src/Components/ProductSection/ProductSection.js
@@ -1,14 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getProductListByCategory } from "../../Request/Requiests";
-import { FaArrowAltCircleUp } from "react-icons/fa";
-import { FaArrowCircleDown } from "react-icons/fa";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
-import { useEffect } from "react";
-import { useState } from "react";
 import ProductDetailsModel from '../../Comman/ProductDetailModel'
 import "../loader.css"
-
-
 import { Card } from "antd";
 
 const { Meta } = Card;
@@ -18,18 +12,36 @@ const ProductSection = () => {
   const category = searchParams.get("category");
   const [catagoryDetails, setCatagoryDetails] = useState([]);
   const [open, setOpen] = useState(false);
-  const [callid,setcallId]= useState("")
-
+  const [callid, setCallId] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getProductListByCategory(category).then((res) => {
-      setCatagoryDetails(res);
-    });
-  }, []);
+    const fetchProductList = async () => {
+      setIsLoading(true);
+      try {
+        const res = await getProductListByCategory(category);
+        if (res === "No Product Found") {
+          setCatagoryDetails([]);
+        } else {
+          setCatagoryDetails(res);
+        }
+      } catch (error) {
+        console.error("Error fetching product list:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  const handleModel = (obj)=>{
-    setOpen(true)
-    setcallId(obj)
+    fetchProductList();
+  }, [category]);
+
+  const handleModel = (obj) => {
+    setOpen(true);
+    setCallId(obj);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>; // Show a loader or a placeholder while loading
   }
 
   return (
@@ -52,47 +64,55 @@ const ProductSection = () => {
           alignItems: "center",
         }}
       >
-        {catagoryDetails.map((p, index) => (
-          <div className="col-3 m-2 ">
-            <Card
-              hoverable
-              style={{ width: 250 }}
-              cover={
-                <img
-                  style={{
-                    height: "300px",
-                    width: "250px",
-                  }}
-                  alt={p.title}
-                  src={p.image}
-                />
-              }
-            >
-<Meta
-              title={p.title}
-            />
-            <div className="d-flex justify-content-between align-items-center my-3">
-              <b>Details:</b>
-              <button type="button" class="btn " data-toggle="modal" onClick={()=>handleModel(p)}  data-target="#exampleModal"> <IoIosArrowDropdownCircle /></button>
+        {catagoryDetails.length === 0 ? (
+          <div>No products available in this category.</div>
+        ) : (
+          catagoryDetails.map((p, index) => (
+            <div key={index} className="col-3 m-2">
+              <Card
+                hoverable
+                style={{ width: 250 }}
+                cover={
+                  <img
+                    style={{
+                      height: "300px",
+                      width: "250px",
+                    }}
+                    alt={p.title}
+                    src={p.image}
+                  />
+                }
+              >
+                <Meta title={p.title} />
+                <div className="d-flex justify-content-between align-items-center my-3">
+                  <b>Details:</b>
+                  <button
+                    type="button"
+                    className="btn"
+                    onClick={() => handleModel(p)}
+                  >
+                    <IoIosArrowDropdownCircle />
+                  </button>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <div>Category:</div>
+                  <b>{p.category}</b>
+                </div>
+                <div className="d-flex justify-content-between total font-weight-bold mt-4">
+                  <span>Total</span>
+                  <span>${p.price}</span>
+                </div>
+              </Card>
             </div>
-            <div className="d-flex justify-content-between">
-              <div>Category:</div>
-              <b>{p.category}</b>
-            </div>
-            <div className="d-flex justify-content-between total font-weight-bold mt-4">
-              <span>Total</span>
-              <span>${p.price}</span>
-            </div>
-            </Card>
-          </div>
-        ))}
+          ))
+        )}
       </div>
-      {open &&
-    <ProductDetailsModel 
-    setOpen={setOpen}
-    obj={callid}
-    />
-    }
+      {open && (
+        <ProductDetailsModel 
+          setOpen={setOpen}
+          obj={callid}
+        />
+      )}
     </div>
   );
 };
