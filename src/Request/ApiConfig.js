74d065624bd94263635b49d8110c5d99cb2b5b77
Parvez Musharraf <parvez.musharraf@pixonix.tech>
2024-08-31 15:57:02 +0530

    change config

--- src/Request/ApiConfig.js
+++ src/Request/ApiConfig.js
@@ -1,5 +1,5 @@
 const MAIN_URL = "https://api.escuelajs.co/api/v1";
-const MAIN_LocalURL = "http://localhost:3000";
+const MAIN_LocalURL = "https://e-commerceapi-1.onrender.com";
 
 export default MAIN_LocalURL
 // export default MAIN_URL 
